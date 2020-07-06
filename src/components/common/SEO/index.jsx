8ece8212aa0bb88ae9ad85212429b3817a4032c0
Ruben Mateus <ruben.mateus@hexis-hub.com>
2020-07-07 00:24:33 +0100

    bootstrap from gatsby

---
+++ src/components/common/SEO/index.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Thumbnail from 'assets/thumbnail/thumbnail.png';
+import {
+  url,
+  defaultDescription,
+  defaultTitle,
+  socialLinks,
+  address,
+  contact,
+  legalName,
+  foundingDate,
+  logo,
+} from 'data/config';
+
+export const SEO = ({ title = defaultTitle, description = defaultDescription, location = '' }) => {
+  const structuredDataOrganization = `{ 
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"legalName": "${legalName}",
+		"url": "${url}",
+		"logo": "${logo}",
+		"foundingDate": "${foundingDate}",
+		"founders": [{
+			"@type": "Person",
+			"name": "${legalName}"
+		}],
+		"contactPoint": [{
+			"@type": "ContactPoint",
+			"email": "${contact.email}",
+			"telephone": "${contact.phone}",
+			"contactType": "customer service"
+		}],
+		"address": {
+			"@type": "PostalAddress",
+			"addressLocality": "${address.city}",
+			"addressRegion": "${address.region}",
+			"addressCountry": "${address.country}",
+			"postalCode": "${address.zipCode}"
+		},
+		"sameAs": [
+			"${socialLinks.linkedin}",
+			"${socialLinks.github}"
+		]
+  	}`;
+
+  return (
+    <Helmet>
+      <meta name="description" content={description} />
+      <meta name="image" content={Thumbnail} />
+
+      <meta property="og:url" content={`${url}${location}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={Thumbnail} />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image:src" content={Thumbnail} />
+
+      <script type="application/ld+json">{structuredDataOrganization}</script>
+      <title>{title}</title>
+      <html lang="en" dir="ltr" />
+    </Helmet>
+  );
+};
---
+++ src/data/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  defaultTitle: 'John Doe',
+  logo: '',
+  author: 'John Doe',
+  url: '',
+  legalName: 'John Doe',
+  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    github: 'https://github.com/',
+    linkedin: 'https://www.linkedin.com/in/',
+  },
+  googleAnalyticsID: '',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2020',
+};
